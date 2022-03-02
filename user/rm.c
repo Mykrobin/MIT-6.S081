a2b6a30e81c2acb18f4d0eba8b38dd6488c8c7f3
Mykrobin <mykrobin@foxmail.com>
2022-03-02 03:59:18 -0800

    update invoke notes

--- user/rm.c
+++ user/rm.c
@@ -7,13 +7,13 @@ main(int argc, char *argv[])
 {
   int i;
 
-  if(argc < 2){
+  if(argc < 2){ // 当输入仅有一个参数时，报错
     fprintf(2, "Usage: rm files...\n");
-    exit(1);
+    exit(1);    // 表示异常退出
   }
 
-  for(i = 1; i < argc; i++){
-    if(unlink(argv[i]) < 0){
+  for(i = 1; i < argc; i++){ // 超过一个输入时，进行相应操作并通过 fprintf() 输出信息
+    if(unlink(argv[i]) < 0){ // unlink() 函数功能是删除文件，通过返回值判断删除是否成功
       fprintf(2, "rm: %s failed to delete\n", argv[i]);
       break;
     }
