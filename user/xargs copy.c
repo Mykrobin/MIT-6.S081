03e13496a944406e1ef1c8ff99eb3c80fdf473d5
Mykrobin <mykrobin@foxmail.com>
2022-03-04 01:18:37 -0800

    update xargs.c

--- user/xargs copy.c
+++ user/xargs copy.c
@@ -1,4 +1,14 @@
-// 时间 2022年3月4日16:42:47
+/**
+ * Created with IntelliJ IDEA.
+ * @Auther: Robin
+ * @Date: 2021/6/24 / 20:24
+ * @Version: 
+ * @Description: TODO MIT-6.S081-util-xargs
+ */
+
+//
+// Created by Lenovo on 2021/6/24.
+//
 
 #include "kernel/types.h"
 #include "kernel/param.h"
@@ -13,32 +23,24 @@ int main(int argc, char *argv[])
     int n, args_index = 0;
     int i;
 
-    // 若输入命令为 echo hello too | xargs echo bye, 则以下输出为
+    // 若输入命令为 echo h t | xargs echo b, 则以下输出为
 //    printf("argv[0] = >%s<\n", argv[0]); // >xargs<
 //    printf("argv[1] = >%s<\n", argv[1]); // >echo<
-//    printf("argv[2] = >%s<\n", argv[2]); // >bye<
+//    printf("argv[2] = >%s<\n", argv[2]); // >b<
 
     char* cmd = argv[1];
 //    printf("cmd = %s\n", cmd); // echo
     for (i = 1; i < argc; i++) params[args_index++] = argv[i];
 //    printf("args_index = >%d< \n", args_index); // >2<
-//    printf("params = >%s< >%s<\n", params[0], params[1]); // >echo< >bye<
+//    printf("params = >%s< >%s<\n", params[0], params[1]); // >echo< >b<
 
     while ((n = read(0, line, MAXLINE)) > 0)
     {
-    //    printf("n = %d\n", n); // 10
-    //    for (i = 0; i<n; i++) printf("line[%d]: %c\n", i, line[i]);
-        // line[0]: h
-        // line[1]: e
-        // line[2]: l
-        // line[3]: l
-        // line[4]: o
-        // line[5]:
-        // line[6]: t
-        // line[7]: o
-        // line[8]: o
-        // line[9]:
-
+//        printf("n = %d\n", n); // 4
+//        printf("line[0] = >%c<\n", line[0]); // >h<
+//        printf("line[1] = >%c<\n", line[1]); // > <
+//        printf("line[2] = >%c<\n", line[2]); // >t<
+//        printf("line[3] = >%c<\n", line[3]); // >\n<
 
         if (fork() == 0) // child process
         {
