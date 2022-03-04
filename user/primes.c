80f412907246932edafe508577df5b116f9274a0
Mykrobin <mykrobin@foxmail.com>
2022-03-03 19:15:34 -0800

    add new prime.c

--- user/primes.c
+++ user/primes.c
@@ -1,14 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * @Auther: Robin
- * @Date: 2021/6/23 / 22:01
- * @Version:
- * @Description: TODO：TODO：MIT-6.S081-util-primes
- */
-
-//
-// Created by Lenovo on 2021/6/23.
-//
+// 时间：2022年3月4日11:15:02
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
@@ -19,50 +9,65 @@
 int
 main(int argc, char *argv[])
 {
-    int j;
-    int val[Max];
-    int pid, p[2], temp, prime=2, index=Max;
+    int i;
+    int temp = 0, num = 0; // 设置最小的素数，当前队列元素个数
+    int pipe_p[2]; // 构建管道
+    int pid;
+    int val[Max]; // 构建数组
 
-    // init
-    for (j = 1; j <= Max; ++j) {
-        val[j] = j+1;
+    // 初始化数组
+    for (i = 1; i < Max; i++)
+    {
+        val[i] = i+1;
+        num ++; // 记录当前数组元素个数
     }
-
-    while (val[1] != 0) {
-
-        pipe(p);  // 注意pipe 与 fork的顺序
+    
+    while (num != 0)
+    {
+        pipe(pipe_p);
         pid = fork();
+        if (0 == pid){ // 子进程
+            close(pipe_p[1]); // 关闭管道写
+            // 从管道读取数据，找到最小素数
+            temp = 0;
+            for (i = 1; i < Max; i++)
+            {   
+                read(pipe_p[0], &val[i], sizeof(int));
+                if (temp==0 && val[i]!=0)
+                {
+                    temp = val[i]; // 确定当前最小素数
+                }
+                
+            }
+            if (0 != temp)
+            {
+                printf("prime %d \n", temp); // 每次遍历打印最小素数，当全部元素为0，停止fork   
+            } else {
+                num = 0;
+                break;
+            }
 
-        if (pid == 0) { // child
-            close(p[1]);
-            index = 0;
-            while (read(p[0], &temp, sizeof(temp)) != 0) { // 找到下一次prime
-                if (index == 0) { // 接收到的第一个数
-                    prime = temp;
-                    index++;
-                } else {
-                    if (temp%prime !=0) { // 筛选
-                        val[index] = temp;
-                        index++;
-                    } else{
-                        val[index] = 0; // 不符合条件的置为0
-                    }
+            // 筛选素数
+            for (i = 1; i < Max; i++)
+            {
+                if (0 == val[i]%temp) {
+                    val[i] = 0;
+                    num--;  // 重置当前数组个数，个数为0 的时候停止fork
                 }
             }
-            printf("prime %d\n", prime);
-            close(p[0]);
-
-        } else { // parent
-            close(p[0]);
-            for ( j = 1; j <= index; ++j) {
-                write(p[1], &val[j], sizeof(val[j]));
+            close(pipe_p[0]);// 关闭管道读
+        } else { // 父进程
+            close(pipe_p[0]); // 关闭管道读
+            for (i = 1; i < Max; i++)
+            {
+                // printf("parent prime: %d \n", val[i]);
+                write(pipe_p[1], &val[i], sizeof(int)); // 写入管道
             }
-            close(p[1]);
-
+            close(pipe_p[1]); // 关闭管道写
             wait(0);
             exit(0);
         }
     }
-
+    
     exit(0);
 }
