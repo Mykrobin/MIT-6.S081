b41ecf347ab24de258e2799a11c0aa0d006f7a48
Mykrobin <mykrobin@foxmail.com>
2022-03-04 00:40:25 -0800

    add new find.c

--- user/find.c
+++ user/find.c
@@ -1,14 +1,5 @@
-/**
- * Created with IntelliJ IDEA.
- * @Auther: Robin
- * @Date: 2021/6/24 / 9:56
- * @Version: 
- * @Description: TODO MIT-6.S081-util-find
- */
-
-//
-// Created by Lenovo on 2021/6/24.
-//
+// 时间：2022年3月4日15:00:25
+// 参考：ls.c 查看如何如何查看目录 
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
@@ -16,103 +7,96 @@
 #include "kernel/fs.h"
 
 char*
-fmtname(char *path)
+fmtname(char *path) // 功能：遍历当前目录下的文件及路径
 {
-    static char buf[DIRSIZ+1];
-    char *p;
+  static char buf[DIRSIZ+1];
+  char *p;
 
-    // Find first character after last slash.
-    for(p=path+strlen(path); p >= path && *p != '/'; p--)
-        ;
-    p++;
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
 
-    // Return blank-padded name.
-    if(strlen(p) >= DIRSIZ)
-        return p;
-    memmove(buf, p, strlen(p));
-    memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-
-    return buf;
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
 }
 
 void
 find(char *path, char *name)
 {
-    char buf[512], *p;
-    int fd;
-    struct dirent de;
-    struct stat st;
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
 
-    if((fd = open(path, 0)) < 0){
-        fprintf(2, "ls: cannot open %s\n", path);
-        return;
-    }
+//   printf("path: >%s< , name: >%s< \n",path, name);
+// 对错误的输入或路径返回对应信息
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
 
-    if(fstat(fd, &st) < 0){
-        fprintf(2, "ls: cannot stat %s\n", path);
-        close(fd);
-        return;
-    }
-
-    switch(st.type){
-
-        case T_FILE:
-            if ( 0 == strcmp(fmtname(path), name)) { // 查找到目标文件
-                printf("path: %s \n", path);
-            }
-            break;
-
-        case T_DIR:
-            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-                printf("ls: path too long\n");
-                break;
-            }
-            strcpy(buf, path);
-            p = buf+strlen(buf);
-            *p++ = '/';
-
-            while(read(fd, &de, sizeof(de)) == sizeof(de)){
-
-                if(de.inum == 0)
-                    continue;
-                memmove(p, de.name, DIRSIZ);
-                p[DIRSIZ] = 0;
-                if(stat(buf, &st) < 0){
-                    printf("ls: cannot stat %s\n", buf);
-                    continue;
-                }
-
-                if (0==strcmp(de.name, name) && st.type==T_FILE) { // 类型为文件, 找到之后输出
-                    printf("%s \n", buf);
-                }
-                /* 以下为各个参数的输出结果，供输出打印参考 */
-//                printf("fmtname(buf) = %s\n", fmtname(buf));  // find
-//                printf("path = %s\n", path);                  // .
-//                printf("buf = %s\n", buf);                    // ./find
-//                printf("de.name = %s\n", de.name);            // find
-//                printf("============================\n");
-
-//                printf("T_DIR = %d, de.name = %s, st.type = %d\n", T_DIR, de.name, st.type);
-                if ((strcmp(de.name, "..")) && (strcmp(de.name, ".")) && st.type==T_DIR) { // 递归进入下一层目录
-//                    printf("-------get into ------ \n");
-                    find(buf, name);
-                }
-            }
-            break;
-    }
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
+    return;
+  }
+
+// 查找对应内容
+  switch(st.type){
+  case T_FILE:  // 查找到文件，直接打印当前目录
+    if (0 == strcmp(fmtname(path), name)) {
+        printf("path: %s \n", path);
+    }
+    break;
+
+  case T_DIR:  // 查找到目录类型则进一步进入下一层子目录进行查找
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("ls: path too long\n"); // 定义文件目录名称长度为14
+      break;
+    }
+    strcpy(buf, path);
+    // printf("buf: >%s< strlen(buf): %d \n", buf, strlen(buf));
+    p = buf+strlen(buf);
+    // printf("p: >%s< \n", p);
+    *p++ = '/';
+
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      
+      if(de.inum == 0) continue;  // dirent 结构体包含 inum 和 名称，inum对应inode号 （fs.h 中定义）
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+
+      if(stat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+
+      // 输出文件类型对应的目录
+      if (0==strcmp(de.name, name) && st.type==T_FILE) {
+          printf("%s \n", buf);
+      }
+      
+      //  当前目录中包含子目录，采用递归方式进行查找
+      if ((strcmp(de.name, ".")) && (strcmp(de.name, "..")) && (st.type==T_DIR))
+        find(buf, name);
+    }
+    break;
+  }
+  close(fd);
 }
 
 int
 main(int argc, char *argv[])
 {
-    if(argc < 2){
-        find(".", "find");
-        fprintf(2, "Usage: find filename.\n");
-        exit(0);
-    }
-
-    find(argv[1], argv[2]);
-
+  if(argc < 2){ // 使用不当
+    fprintf(2, "Usage: find filename. \n");
     exit(0);
+  }
+  find(argv[1], argv[2]); // 分别对应路径名称和文件名称
+  exit(0);
 }
